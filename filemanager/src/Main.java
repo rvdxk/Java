1c729aad2152adac2d59aafd870b3184e46d4920
rvdxk <radek.szczot@gmail.com>
2023-09-29 12:39:49 +0200

    File Manager

---
+++ filemanager/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        FileManager fileManager = new FileManager();
+        fileManager.sortFilesInDirectory("C:\\Users\\Public\\Pictures");
+    }
+}
